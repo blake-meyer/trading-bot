60206e0d24c9b59b314facd0c291a510c77b702d
blake-meyer <98920800+blake-meyer@users.noreply.github.com>
2022-02-08 10:37:11 -0600

    Initial commit
    Ability to check current BTC price with Binance Spot Test API

---
+++ PriceTicker.cs
@@ -0,0 +1,8 @@
+namespace BitcoinBot
+{
+    public class PriceTicker
+    {
+        public string? symbol {get; set;}
+        public string? price {get; set;}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Binance.Spot;
+using System.Text.Json;
+//using BitcoinBot;
+
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+namespace BitcoinBot
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string apiKey = "ZxNHyzK4aOz2lWoyWYFbozgJ3N0GvRr17LPMAimMiv8VRIuSZWImlKlSEEKcChtU";
+            string secretKey = "K3Hupa1kBrUyLgQn2dMpeoZtlQnbtICAZFkEIt8feajb5xLycoODuf4oYRiUQ6U8";
+
+                        
+
+            Market market = new Market("https://testnet.binance.vision", apiKey, secretKey);
+
+            var priceTickerString = await market.SymbolPriceTicker("BTCUSDT");
+
+
+            PriceTicker? priceTickerObj = JsonSerializer.Deserialize<PriceTicker>(priceTickerString);
+
+            if (priceTickerObj?.price != null)
+            {
+                Console.WriteLine("BTC Price: " + priceTickerObj.price);
+            }
+        }
+    }
+}
+
+
