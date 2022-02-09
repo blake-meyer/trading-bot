f219e88f728bc9e1599d27acd4a77a2e4cf4153c
Blake Meyer <blake.meyer145@gmail.com>
2022-02-08 18:33:32 -0600

    Add calculations

--- Program.cs
+++ Program.cs
@@ -13,11 +13,15 @@ namespace BitcoinBot
         {
             var config = new ConfigurationBuilder()
                 .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json", false)
                 .AddUserSecrets<Program>()
                 .Build();
 
             string apiKey = config["apiKey"];
             string secretKey = config["secretKey"];
+            decimal coreNumber = System.Convert.ToDecimal(config["coreNumber"]);
+            decimal marginPercent = System.Convert.ToDecimal(config["marginPercent"]);
+            string action = "None";
 
             string baseUrl = "https://testnet.binance.vision";
 
@@ -39,13 +43,36 @@ namespace BitcoinBot
             var accountInformationString = await account.AccountInformation();
             AccountInformation? accountInformation = JsonSerializer.Deserialize<AccountInformation>(accountInformationString);
 
+            decimal currentBTCHolding = 0;
+            decimal changePercent = 0;
+            decimal tradeAmount = 0;
             foreach (Balance balance in accountInformation?.balances)
             {
-                if (balance.asset == "BTC" | balance.asset == "USDT")
+                if (balance.asset == "BTC")
                 {
+                    currentBTCHolding = System.Convert.ToDecimal(balance.free) * System.Convert.ToDecimal(priceTickerObj.price);
+                    changePercent = (currentBTCHolding - coreNumber) / coreNumber * 100;
+                    tradeAmount = Math.Abs(currentBTCHolding - coreNumber);
+                    //trade_amount    = round(abs(current_holding - my_core_number), my_round_off)
+                    
                     Console.WriteLine(balance.asset + " Balance: " + balance.free);
                 }
             }
+
+            Console.WriteLine("Current BTC Holding: " + currentBTCHolding);
+            Console.WriteLine("Change Percent: " + changePercent);
+            
+            if (changePercent > marginPercent)
+            {
+                action = "Sell";
+            }
+            else if (changePercent < marginPercent)
+            {
+                action = "Buy";
+            }
+            Console.WriteLine("ACTION: " + action);
+            Console.WriteLine("Trade Amount: " + tradeAmount);
+
         }
     }
 }
